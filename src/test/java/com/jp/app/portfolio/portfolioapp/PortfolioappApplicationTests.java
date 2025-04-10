7e4e4b38e154ea4f6e7979be9465aa81c5aae116
Jeevan <cyanide595@gmail.com>
2025-04-10 20:34:40 +0545

    Initiate project

---
+++ src/test/java/com/jp/app/portfolio/portfolioapp/PortfolioappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.app.portfolio.portfolioapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
